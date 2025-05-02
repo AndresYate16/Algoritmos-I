d206a19edd398a593d60327580e6c9f98884e1c8
AndresYate16 <andresyate2016@gmail.com>
2025-05-02 12:52:35 -0500

    Actualizacion de carpetas

--- Momento_2/Ejercicio_1/calculadora.java
+++ Momento_2/Ejercicio_1/calculadora.java
@@ -1,84 +1,89 @@
-import com.sun.jdi.Value;
 import java.util.Scanner;
 
-public class calculadora {
+public class CalculadoraDoWhile {
+
     public static void main(String[] args) {
-        int numA = 0, numB = 0;
-        int varControl = 0;
-        int Factorial = 1;
+        Scanner scanner = new Scanner(System.in);
 
-        scanner scanner = new scanner(System.in);
+        int OpcUser = 0, numA = 0, numB = 0;
+        boolean Key = true;
 
-        System.out.println("Bienvenidos");
-        while (varControl != 6) { 
-            
-        }
-        System.out.println("1. sumar dos números\r\n" + //
-                        "2. Restar dos números\r\n" + // 
-                        "3. Multiplicar dos números\r\n" + // 
-                        "4. Dividir dos números\r\n" + // 
-                        "5. Factorial de un número\r\n" + //
-                        "6. Salir\r\n");
-                        System.err.println("Opcion seleccionada");
-                        varControl = scanner.nextInt ();
-                        
-                        switch (varControl) {
-                            case 1:
-                            System.out.println("Ingrese el número A");
-                            numA = scanner.nextInt();
-                            System.out.println("Ingrese el número B");
-                            numB = scanner.nextInt();
-                            System.out.println(numA + " + " + numB + " = " + (numA+numB));
-                                
-                                break;
-                                case 2:
-                            System.out.println("Ingrese el número A");
-                            numA = scanner.nextInt();
-                            System.out.println("Ingrese el número B");
-                            numB = scanner.nextInt();
-                            System.out.println(numA + " - " + numB + " = " + (numA-numB));
+        do {
+            System.out.println("1. Sumar dos números");
+            System.out.println("2. Restar dos números");
+            System.out.println("3. Multiplicar dos números");
+            System.out.println("4. Dividir dos números");
+            System.out.println("5. Factorial de un número");
+            System.out.println("6. Salir");
+            System.out.print("Opción seleccionada: ");
+            OpcUser = scanner.nextInt();
 
-                            break;
-                            case 3:
-                        System.out.println("Ingrese el número A");
-                        numA = scanner.nextInt();
-                        System.out.println("Ingrese el número B");
-                        numB = scanner.nextInt();
-                        System.out.println(numA + " * " + numB + " = " + (numA*numB));
-
-                            break;
-                            case 4:
-                        System.out.println("Ingrese el número A");
-                        numA = scanner.nextInt();
-                        System.out.println("Ingrese el número B");
-                        numB = scanner.nextInt();
-                        System.out.println(numA + " / " + numB + " = " + (numA/numB));
-
-                        break;
-                        case 5:
-                    System.out.println("Ingrese un número para calcular factorial");
+            switch (OpcUser) {
+                case 1:
+                    System.out.print("Ingrese número A: ");
                     numA = scanner.nextInt();
-                  int contador = numA;
-                  while (contador >1) { 
-                    Factorial *= contador;
-                    contador -= 1;
-                      
-                  }
-                    System.out.println("El factorial de " + numA + numB + " es " + Factorial);
+                    System.out.print("Ingrese número B: ");
+                    numB = scanner.nextInt();
+                    System.out.println(numA + " + " + numB + " = " + (numA + numB));
+                    break;
 
-                        break;
-                            case 6:
-                        System.out.println("Gracias, hasta pronto!");
+                case 2:
+                    System.out.print("Ingrese número A: ");
+                    numA = scanner.nextInt();
+                    System.out.print("Ingrese número B: ");
+                    numB = scanner.nextInt();
+                    System.out.println(numA + " - " + numB + " = " + (numA - numB));
+                    break;
 
-                        break;
+                case 3:
+                    System.out.print("Ingrese número A: ");
+                    numA = scanner.nextInt();
+                    System.out.print("Ingrese número B: ");
+                    numB = scanner.nextInt();
+                    System.out.println(numA + " * " + numB + " = " + (numA * numB));
+                    break;
 
-                            default:
-                            System.out.println("Opción no valida");
-                               break;
-                        }
+                case 4:
+                    System.out.print("Ingrese número A: ");
+                    numA = scanner.nextInt();
+                    System.out.print("Ingrese número B: ");
+                    numB = scanner.nextInt();
+                    if (numB != 0) {
+                        System.out.println(numA + " / " + numB + " = " + (numA / numB));
+                    } else {
+                        System.out.println("Error: No se puede dividir por cero.");
+                    }
+                    break;
 
+                case 5:
+                    System.out.print("Ingrese un número para calcular el factorial: ");
+                    int numero = scanner.nextInt();
+                    if (numero < 0) {
+                        System.out.println("Error: el factorial no está definido para números negativos.");
+                    } else {
+                        long factorial = 1;
+                        int i = 1;
+                        do {
+                            factorial *= i;
+                            i++;
+                        } while (i <= numero);
+                        System.out.println("El factorial de " + numero + " es " + factorial);
+                    }
+                    break;
+
+                case 6:
+                    System.out.println("Gracias, hasta pronto!");
+                    Key = false;
+                    break;
+
+                default:
+                    System.out.println("Opción no válida!");
+                    break;
+            }
+        } while (Key);
 
         scanner.close();
     }
 }
+
 
